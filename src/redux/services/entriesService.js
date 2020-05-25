43f0d296dcb4bcdebe0e16963f6c070e68429f36
Yoann Delion <yoanndelion@gmail.com>
2020-05-25 16:00:10 +0200

    Fix error of feeling name undefined after new entry creation

--- src/redux/services/entriesService.js
+++ src/redux/services/entriesService.js
@@ -2,6 +2,7 @@ import { loadingUi, stopLoadingUi } from '../slices/uiSlice'
 import { addNewEntry } from '../slices/entriesSlice'
 import axios from 'axios'
 import { fetchedAllEntries } from '../slices/entriesSlice'
+import store from '../store'
 
 /**
  * Post a new entry
@@ -12,7 +13,12 @@ export const postNewEntry = entry => async dispatch => {
     dispatch(loadingUi())
     try {
         await axios.post('/entry', entry)
-          .then(response => response.data)
+            .then(response => response.data)
+
+        //todo: refacto
+        const feeling = store.getState().feelings.primaryFeelings.find(feeling => feeling.feelingId === entry.feelingId)
+        entry.feeling = feeling
+
         dispatch(addNewEntry(entry))
         //todo update entry
         dispatch(stopLoadingUi())
@@ -30,7 +36,7 @@ export const fetchAllEntries = () => async dispatch => {
     dispatch(loadingUi())
     try {
         const entries = await axios.get('/entries')
-          .then(response => response.data)
+            .then(response => response.data)
         dispatch(fetchedAllEntries(entries))
         dispatch(stopLoadingUi())
     } catch (e) {
