b1eff4b455aaeaa9f1e0d8cf800b791aa7346f07
Yoann Delion <yoanndelion@gmail.com>
2020-06-24 12:13:08 +0200

    Statspage: Added arrows to change selected month more easily

--- src/Components/Pages/StatsPage.js
+++ src/Components/Pages/StatsPage.js
@@ -4,19 +4,26 @@ import { connect } from 'react-redux'
 import { selectMonthForStatistics } from '../../redux/services/entriesService'
 import { selectedMonthEntriesSelector } from '../../redux/selectors/entriesSelector'
 import Calendar from '../Calendar'
+import moment from 'moment'
 // MUI
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import FeelingsStatsList from '../FeelingsStatsList'
+import IconButton from '@material-ui/core/IconButton'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 
 const StatsPage = ({ selectMonthForStatistics, selectedMonth, entries, primaryFeelings }) => {
 
     const handleDateChange = date => selectMonthForStatistics(date)
 
     return (
-        <Container maxWidth='sm'>
+        <Container maxWidth='sm' className='statspage'>
             <Typography variant='h4' component='h1'>Statistics</Typography>
             <div className='monthPicker'>
+                <IconButton onClick={() => handleDateChange(moment(selectedMonth).subtract(1, 'month'))}>
+                    <ChevronLeftIcon />
+                </IconButton>
                 <DatePicker
                     autoOk
                     views={["month"]}
@@ -33,6 +40,11 @@ const StatsPage = ({ selectMonthForStatistics, selectedMonth, entries, primaryFe
                     onChange={handleDateChange}
                     disableFuture
                 />
+                <IconButton disabled={moment(selectedMonth).isSame(moment(), 'month')} onClick={() => handleDateChange(moment(selectedMonth).add(1, 'month'))}>
+                    <ChevronRightIcon />
+                </IconButton>
+            </div>
+            <div className="statspage--values">
                 <Calendar selectedMonth={new Date(selectedMonth)} entries={entries} />
                 <FeelingsStatsList primaryFeelings={primaryFeelings} entries={entries} />
             </div>
