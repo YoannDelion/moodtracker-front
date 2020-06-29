dd5b9eaa335bf565a5f66c25279443ee402690d7
Yoann Delion <yoanndelion@gmail.com>
2020-06-29 15:44:34 +0200

    Updates

---
+++ precache-manifest.d03692370f56548472381d412db0be6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b191a131ebbe3a5bdd1ef0b404109db2",
+    "url": "/moodtracker-front/index.html"
+  },
+  {
+    "revision": "e9f1e941e57a7475dcf7",
+    "url": "/moodtracker-front/static/css/2.ddfd451b.chunk.css"
+  },
+  {
+    "revision": "ed7f1fbbf7e301541af9",
+    "url": "/moodtracker-front/static/css/main.0a7f9d5c.chunk.css"
+  },
+  {
+    "revision": "e9f1e941e57a7475dcf7",
+    "url": "/moodtracker-front/static/js/2.c0b90c5f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/moodtracker-front/static/js/2.c0b90c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7f1fbbf7e301541af9",
+    "url": "/moodtracker-front/static/js/main.65992472.chunk.js"
+  },
+  {
+    "revision": "a9e8d6656a2cf468897d",
+    "url": "/moodtracker-front/static/js/runtime-main.e0e2f289.js"
+  },
+  {
+    "revision": "c094ca9153b039b05c1930ce7c1b1541",
+    "url": "/moodtracker-front/static/media/Angry.c094ca91.svg"
+  },
+  {
+    "revision": "d7c090864775e4ab3e78402b412cbc7b",
+    "url": "/moodtracker-front/static/media/Fear.d7c09086.svg"
+  },
+  {
+    "revision": "b1e5e77711d6c8eb301aeed62d730208",
+    "url": "/moodtracker-front/static/media/Happy.b1e5e777.svg"
+  },
+  {
+    "revision": "6fa11d011f7a547be857f3cb9cd7cab3",
+    "url": "/moodtracker-front/static/media/Neutral.6fa11d01.svg"
+  },
+  {
+    "revision": "b6957d1c5893a61530fa0ef2500c6fef",
+    "url": "/moodtracker-front/static/media/Sad.b6957d1c.svg"
+  }
+]);
