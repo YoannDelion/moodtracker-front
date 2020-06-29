a2a90b86e91c43121af3c45bda159067a9868a54
Yoann Delion <yoanndelion@gmail.com>
2020-06-29 12:27:45 +0200

    Signup page design

--- src/Components/Pages/SignupPage.js
+++ src/Components/Pages/SignupPage.js
@@ -3,13 +3,18 @@ import axios from 'axios'
 import { connect } from 'react-redux'
 import { loadingUi, stopLoadingUi } from '../../redux/slices/uiSlice'
 import { Link } from 'react-router-dom'
+import { Happy } from '../../Assets/mood-icons/index'
+
 //Material UI
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
-
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import LockIcon from '@material-ui/icons/Lock'
 
 const SignupPage = ({ history, isLoading, dispatch }) => {
 
@@ -39,25 +44,55 @@ const SignupPage = ({ history, isLoading, dispatch }) => {
     return (
         <Grid container maxWidth='sm'>
             <div className='formContainer'>
-                <Typography variant='h4' component='h1'>Signup Page</Typography>
+
+                <img src={Happy} />
+                <Typography variant='h4' component='h1'>Welcome! Sign up here</Typography>
 
                 <form className='form' onSubmit={handleSubmit} noValidate>
                     <TextField id="email" name='email' label="Email" type='email' onChange={handleChange}
                         value={newUser.email} fullWidth className='textField'
                         helperText={errors.email} error={!!errors.email}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <DraftsIcon />
+                                </InputAdornment>
+                            ),
+                        }}
                     />
                     <TextField id="firstName" name='firstName' label="First name" type='text' onChange={handleChange}
                         value={newUser.firstName} fullWidth className='textField'
                         helperText={errors.firstName} error={!!errors.firstName}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <AccountCircleIcon />
+                                </InputAdornment>
+                            ),
+                        }}
                     />
                     <TextField id="password" name='password' label="Password" type='password' onChange={handleChange}
                         value={newUser.password} fullWidth className='textField'
                         helperText={errors.password} error={!!errors.password}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LockIcon />
+                                </InputAdornment>
+                            ),
+                        }}
                     />
                     <TextField id="confirmPassword" name='confirmPassword' label="Confirm Password" type='password'
                         onChange={handleChange}
                         value={newUser.confirmPassword} fullWidth className='textField'
                         helperText={errors.confirmPassword} error={!!errors.confirmPassword}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LockIcon />
+                                </InputAdornment>
+                            ),
+                        }}
                     />
                     {errors.general &&
                         <Typography variant='body2' className='customError'>{errors.general}</Typography>}
@@ -67,8 +102,7 @@ const SignupPage = ({ history, isLoading, dispatch }) => {
                       {isLoading && <CircularProgress size={30} className='progress' />}
                     </Button>
                 </form>
-                <Typography variant={'caption'}>Already have an account ? Log in <Link
-                    to={'/login'}>here</Link></Typography>
+                <Button className='button' component={Link} to={'/login'} variant='text'>Already have an account ? Log in here</Button>
             </div>
         </Grid>
     )
